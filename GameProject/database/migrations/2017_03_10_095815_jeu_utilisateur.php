d52b393b4a63b240f74e1ed8bd437513860ed09c
Baptiste <Baptiste@DESKTOP-74C0K1G>
2017-03-10 11:55:35 +0100

    migration jeu utilisateur

---
+++ GameProject/database/migrations/2017_03_10_095815_jeu_utilisateur.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class JeuUtilisateur extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schemo::create('jeu_utilisateur', function(Blueprint $table){
+            $table->increments('id');
+            $table->boolean('actif')->default(true);
+            $table->integer('jeu_id')->unsigned();
+            $table->foreign('jeu_id')->references('id')->on('jeus');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->engine='InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
