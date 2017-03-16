0fd2b9da1674945b35612640ff2ef7f716161bb3
REQJ <weshicipapa@gmail.com>
2017-03-16 19:20:36 +0100

    namespace, nom typejeus

--- GameProject/app/Models/Jeu.php
+++ GameProject/app/Models/Jeu.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,7 +11,7 @@ class Jeu extends Model
      	return $this->hasMany('App\Models\Sujet');
      }
     
-    public function type_jeus()
+    public function typeJeus()
      {
      	return $this->belongsTo('App\Models\Type_Jeu');
      }
